f71385eb8520c445a29eb72a40e5b7a23172a0c3
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2024-01-08 21:53:23 +0530

    Print 5 to 1

---
+++ src/com/saurabh/javabasics/Print5to1UsingWhile.java
@@ -0,0 +1,11 @@
+package com.saurabh.javabasics;
+
+public class Print5to1UsingWhile {
+    public static void main(String[] args) {
+        int num = 5;
+        while (num > 0) {
+            System.out.print(num + " ");
+            num--;
+        }
+    }
+}
